ca958ba455edeac76b1cf6268e512b76e8557901
Chris Meek <chrism@agilebits.com>
2017-06-20 12:10:38 -0400

    Use ParseIP to validate IP addresses

--- remote.go
+++ remote.go
@@ -1,6 +1,7 @@
 package remote
 
 import (
+	"fmt"
 	"net"
 	"net/http"
 	"strings"
@@ -15,30 +16,40 @@ type IP struct {
 }
 
 // GetIP returns the remote IP for the given http.Request. It checks optional CF-Connecting-IP and X-FORWARDED-FOR headers.
-func GetIP(r *http.Request) *IP {
+func GetIP(r *http.Request) (*IP, error) {
 	info := &IP{}
 
 	cloudflareIP := r.Header.Get("CF-Connecting-IP")
 	if len(cloudflareIP) > 0 {
-		parseIPs(cloudflareIP, info)
-		return info
+		if err := parseIPs(cloudflareIP, info); err != nil {
+			return nil, fmt.Errorf("GetIP failed to parseIPs %q", cloudflareIP)
+		}
+		return info, nil
 	}
 
 	xff := r.Header.Get("X-FORWARDED-FOR")
 	if len(xff) > 0 {
-		parseIPs(xff, info)
-		return info
+		if err := parseIPs(xff, info); err != nil {
+			return nil, fmt.Errorf("GetIP failed to parseIPs %q", xff)
+		}
+		return info, nil
 	}
 
 	info.IPCount = 1
 	info.IP1, _, _ = net.SplitHostPort(r.RemoteAddr)
 	info.IPLast = info.IP1
 
-	return info
+	return info, nil
 }
 
-func parseIPs(s string, info *IP) {
+func parseIPs(s string, info *IP) error {
 	ips := strings.Split(s, ",")
+
+	for _, ip := range ips {
+		if net.ParseIP(ip) == nil {
+			return fmt.Errorf("parseIPs failed to net.ParseIP %q", ip)
+		}
+	}
 
 	info.IPCount = int8(len(ips))
 	info.IP1 = strings.TrimSpace(ips[0])
@@ -47,4 +58,6 @@ func parseIPs(s string, info *IP) {
 	}
 
 	info.IPLast = strings.TrimSpace(ips[len(ips)-1])
+
+	return nil
 }
